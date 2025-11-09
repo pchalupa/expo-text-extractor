3982406edcf18285c7c67443efdaae5e065b1ef2
pchalupa <chalupa.petr93@gmail.com>
2025-11-09 15:37:12 +0100

    fix: uri parsing

--- android/src/main/java/expo/modules/textextractor/ExpoTextExtractorModule.kt
+++ android/src/main/java/expo/modules/textextractor/ExpoTextExtractorModule.kt
@@ -8,6 +8,7 @@ import expo.modules.kotlin.Promise
 import expo.modules.kotlin.exception.CodedException
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import androidx.core.net.toUri
 import java.io.File
 
 class ExpoTextExtractorModule : Module() {
@@ -21,14 +22,10 @@ class ExpoTextExtractorModule : Module() {
     AsyncFunction("extractTextFromImage") { uriString: String, promise: Promise ->
       try {
         val context = appContext.reactContext!!
-        val uri = if (uriString.startsWith("content://")) {
-          Uri.parse(uriString)
-        } else {
-          val file = File(uriString)
-          if (!file.exists()) {
-            throw Exception("File not found: $uriString")
-          }
-          Uri.fromFile(file)
+        val uri = when {
+          uriString.startsWith("file://") -> uriString.toUri()
+          uriString.startsWith("content://") -> uriString.toUri()
+          else -> Uri.fromFile(File(uriString))
         }
 
         val inputImage = InputImage.fromFilePath(context, uri)
